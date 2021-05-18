3563905e48cb3432f7fc0e943858ff0746877759
Wi1mer <wilmerdiazolaya@gmail.com.com>
2021-05-18 00:02:01 -0500

    V-1.0

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev',));
+app.use(morgan(':remote-user :date :remote-addr :user-agent :res[content-length]'));
+
+// routes
+app.use(require('./routes'));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// listening the Server
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'));
+});
